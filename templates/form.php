bfa5ebcaca54fe89331c53ef3807b09c316ac7d5
Nicolas Gomez <nicogomezgarcia1@gmail.com>
2023-11-07 11:59:08 +0100

    forms poco a poco

---
+++ templates/form.php
@@ -0,0 +1,8 @@
+<form action="form" method="post">
+<label for = "dni">DNI:</label>
+<imput type = "text" name = "dni" required></imput>
+<button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
+<p>nynynmymtymkutmu <a href="login.php">hbtrgngtd</a></p>
