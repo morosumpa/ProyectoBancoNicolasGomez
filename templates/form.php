d34f5943995e6a5d871d906cfc5287fd1473ef9a
Nicolas Gomez <nicogomezgarcia1@gmail.com>
2023-12-01 09:30:14 +0100

    cambios de estructura

--- templates/form.php
+++
@@ -1,8 +0,0 @@
-<form action="form" method="post">
-<label for = "dni">DNI:</label>
-<imput type = "text" name = "dni" required></imput>
-<button type="submit" class="btn btn-primary">Submit</button>
-</form>
-
-
-<p>nynynmymtymkutmu <a href="login.php">hbtrgngtd</a></p>
