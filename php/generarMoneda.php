28acc0c18691ee3ef395305213f51df7951740e4
Nicolás Gómez García <nicogomezgarcia1@gmail.com>
2023-12-12 10:06:14 +0100

    realizacion del cambio de moneda de todas a todas

--- php/generarMoneda.php
+++ php/generarMoneda.php
@@ -1,5 +1,8 @@
 <?php
-// Conexión a la base de datos (ajusta según tus credenciales)
+session_start();
+$DNI_usuario = $_SESSION['DNI'];
+
+// Conexión a la base de datos
 $conexion = new mysqli("localhost", "root", "", "DisBank");
 
 // Verificar la conexión
@@ -11,63 +14,50 @@ if ($conexion->connect_error) {
 $monedaOrigen = strtoupper($_POST['monedaOrigen']);
 $monedaDestino = strtoupper($_POST['monedaDestino']);
 
-// Obtener el IBAN del usuario (ajusta según tu lógica de usuario)
-$ibanUsuario = "IBAN_DEL_USUARIO_ACTUAL";
+// Obtener el IBAN del usuario desde la tabla Cuenta
+$queryIbanUsuario = "SELECT IBAN FROM Cuenta WHERE ID_usuario = (SELECT ID FROM Usuario WHERE DNI = ?)";
+$stmtIbanUsuario = $conexion->prepare($queryIbanUsuario);
 
-// Obtener el valor de cambio de la base de datos o insertar si no existe
-$query = "SELECT Tasa_cambio FROM CambioMoneda WHERE UPPER(Moneda_origen) = UPPER(?) AND UPPER(Moneda_destino) = UPPER(?)";
-$stmt = $conexion->prepare($query);
-
-if (!$stmt) {
-    die("Error al preparar la consulta: " . $conexion->error);
+if (!$stmtIbanUsuario) {
+    die("Error al preparar la consulta para obtener el IBAN del usuario: " . $conexion->error);
 }
 
-$stmt->bind_param("ss", $monedaOrigen, $monedaDestino);
+$stmtIbanUsuario->bind_param("s", $DNI_usuario);
 
-if (!$stmt->execute()) {
-    die("Error al ejecutar la consulta: " . $stmt->error);
+if (!$stmtIbanUsuario->execute()) {
+    die("Error al ejecutar la consulta para obtener el IBAN del usuario: " . $stmtIbanUsuario->error);
 }
 
-$stmt->bind_result($tasaCambio);
+$stmtIbanUsuario->bind_result($ibanUsuario);
 
-// Si no hay resultados, insertar un nuevo registro
-if (!$stmt->fetch()) {
-    $stmt->close();
-
-    // Valor de cambio por defecto
-    $valorCambioPorDefecto = 1;
-
-    // Insertar nuevo registro
-    $insertQuery = "INSERT INTO CambioMoneda (Moneda_origen, Moneda_destino, Tasa_cambio) VALUES (?, ?, ?)";
-    $insertStmt = $conexion->prepare($insertQuery);
-
-    if (!$insertStmt) {
-        die("Error al preparar la consulta de inserción: " . $conexion->error);
-    }
-
-    $insertStmt->bind_param("ssd", $monedaOrigen, $monedaDestino, $valorCambioPorDefecto);
-
-    if (!$insertStmt->execute()) {
-        die("Error al insertar el nuevo registro: " . $insertStmt->error);
-    }
-
-    $insertStmt->close();
-
-    // Actualizar tasa de cambio
-    $tasaCambio = $valorCambioPorDefecto;
+// Verificar si se obtuvo el IBAN
+if (!$stmtIbanUsuario->fetch()) {
+    die("No se encontró un IBAN para el usuario. DNI del usuario: $DNI_usuario");
 }
 
-$stmt->close();
+$stmtIbanUsuario->close();
+
+// Definir las tasas de cambio fijas
+$tasasDeCambio = [
+    "EURO" => ["DOLAR" => 1.1, "LIBRA" => 0.9, "YEN" => 160, "RUBLO" => 95],
+    "DOLAR" => ["EURO" => 1 / 1.1, "LIBRA" => 0.9 / 1.1, "YEN" => 160 / 1.1, "RUBLO" => 95 / 1.1],
+    "LIBRA" => ["EURO" => 1 / 0.9, "DOLAR" => 1.1 / 0.9, "YEN" => 160 / 0.9, "RUBLO" => 95 / 0.9],
+    "YEN" => ["EURO" => 1 / 160, "DOLAR" => 1.1 / 160, "LIBRA" => 0.9 / 160, "RUBLO" => 95 / 160],
+    "RUBLO" => ["EURO" => 1 / 95, "DOLAR" => 1.1 / 95, "LIBRA" => 0.9 / 95, "YEN" => 160 / 95],
+];
+
+// Obtener la tasa de cambio
+$tasaCambio = $tasasDeCambio[$monedaOrigen][$monedaDestino];
 
 // Obtener el saldo actual en la moneda de origen
-$querySaldo = "SELECT Saldo FROM Cuenta WHERE IBAN = ? AND Tipo_cuenta = ?";
+$querySaldo = "SELECT Saldo FROM Cuenta WHERE IBAN = ?";
 $stmtSaldo = $conexion->prepare($querySaldo);
 
 if (!$stmtSaldo) {
     die("Error al preparar la consulta de saldo: " . $conexion->error);
 }
 
-$stmtSaldo->bind_param("ss", $ibanUsuario, $monedaOrigen);
+$stmtSaldo->bind_param("s", $ibanUsuario);
 
 if (!$stmtSaldo->execute()) {
     die("Error al ejecutar la consulta de saldo: " . $stmtSaldo->error);
@@ -86,14 +76,14 @@ $stmtSaldo->close();
 $saldoConvertido = $saldo * $tasaCambio;
 
 // Actualizar el saldo en la moneda de destino
-$queryActualizarSaldo = "UPDATE Cuenta SET Saldo = ? WHERE IBAN = ? AND Tipo_cuenta = ?";
+$queryActualizarSaldo = "UPDATE Cuenta SET Saldo = ? WHERE IBAN = ?";
 $stmtActualizarSaldo = $conexion->prepare($queryActualizarSaldo);
 
 if (!$stmtActualizarSaldo) {
     die("Error al preparar la consulta de actualización de saldo: " . $conexion->error);
 }
 
-$stmtActualizarSaldo->bind_param("dss", $saldoConvertido, $ibanUsuario, $monedaDestino);
+$stmtActualizarSaldo->bind_param("ds", $saldoConvertido, $ibanUsuario);
 
 if (!$stmtActualizarSaldo->execute()) {
     die("Error al actualizar el saldo: " . $stmtActualizarSaldo->error);
@@ -105,5 +95,5 @@ $stmtActualizarSaldo->close();
 $conexion->close();
 
 // Aquí puedes redirigir o mostrar un mensaje de éxito
-echo "El saldo ha sido convertido correctamente. Tasa de cambio: $tasaCambio";
+echo "El saldo ha sido convertido correctamente. Tasa de cambio: $tasaCambio. Cantidad inicial: $saldo. Cantidad convertida: $saldoConvertido. IBAN: $ibanUsuario";
 ?>
